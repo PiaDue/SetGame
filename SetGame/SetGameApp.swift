cb4de3e49286b9252b6d0cc58ac5e445e777ffa3
PiaDue <pia.duenow@web.de>
2022-11-20 13:52:27 +0100

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Pia Dünow on 20.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
