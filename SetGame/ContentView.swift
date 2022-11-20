48dbab474ab81920eb225f7d2932f4a39c8c366a
PiaDue <pia.duenow@web.de>
2022-11-20 21:59:03 +0100

    created model file

--- SetGame/ContentView.swift
+++ SetGame/ContentView.swift
@@ -4,16 +4,28 @@
 //
 //  Created by Pia Dünow on 20.11.22.
 //
+// View
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            //cards
+            
+            Spacer()
+            
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10).foregroundColor(.accentColor)
+                    Text("+3 Cards")
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10).foregroundColor(.accentColor)
+                    Text("New Game")
+                }
+            }
         }
         .padding()
     }
@@ -22,5 +34,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 mini")
     }
 }
---
+++ SetGame/GameLogic.swift
@@ -0,0 +1,34 @@
+//
+//  GameLogic.swift
+//  SetGame
+//
+//  Created by Pia Dünow on 20.11.22.
+//
+
+import Foundation
+
+struct GameLogic {
+    private(set) var cards: Array<Card>
+    private var indiciesOfSelectedCards: Array<Int>
+    
+    init(cards: Array<Card>, indiciesOfSelectedCards: Array<Int>) {
+        self.cards = cards
+        self.indiciesOfSelectedCards = indiciesOfSelectedCards
+    }
+    
+    struct Card {
+        let id: Int
+        var isMatched: Bool = false
+        let content: SetCardContent
+        
+    }
+}
+
+struct SetCardContent {
+    /*
+     let color: new three-value-datatype (using enum?)
+     let shape:
+     let shading:
+     */
+}
+
